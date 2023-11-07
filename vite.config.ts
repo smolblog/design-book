b45894333b1d8d1b7660428386a408311d03e7ee
Evan Hildreth <me@eph.me>
2023-11-06 22:18:17 -0500

    Add LightningCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
+import browserslist from 'browserslist';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { browserslistToTargets } from 'lightningcss';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+	},
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%'))
+    }
+  },
+  build: {
+    cssMinify: 'lightningcss'
+  }
 });
