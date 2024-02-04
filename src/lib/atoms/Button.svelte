04c85b318d1caa86459024124c618fb71ec2fafa
Evan Hildreth <me@eph.me>
2024-02-04 18:13:18 -0500

    Some ideas

--- src/lib/atoms/Button.svelte
+++ src/lib/atoms/Button.svelte
@@ -1,33 +1,37 @@
 <script lang="ts">
+	import type { SemanticColor } from "$lib/types.js";
+
 	export let type: 'submit'|'reset'|null = null;
-	export let theme: 'secondary'|'primary'|'danger' = 'secondary';
+	export let theme: SemanticColor|null = null;
 	export let disabled = false;
 </script>
 
 <style>
 	button {
 		font-size: 1rem;
-		border-radius: 0;
-		border: .1rem solid var(--color-secondary);
+		border-radius: .5em;
+		border: 0;
 		padding: .5em 1em;
 		display: inline-block;
-		background: var(--color-secondary-background);
+		background: var(--neutral-gradient-light);
 		font-weight: bold;
-		color: var(--color-foreground);
-		box-shadow: 0 .2rem .4rem .2rem rgba(0, 0, 0, 0.3);
+		color: var(--body-content-dark);
+		box-shadow: 0 .25rem .25rem .125rem rgba(0, 0, 0, 0.2);
 
 		&:active {
-			box-shadow: 0 .1rem .2rem .1rem rgba(0, 0, 0, 0.3);
+			box-shadow: 0 .125rem .125rem .0625rem rgba(0, 0, 0, 0.2);
 		}
 		
 		&.primary {
-			background: var(--color-primary);
-			color: white;
+			background: var(--primary-gradient-light);
+		}
+		
+		&.secondary {
+			background: var(--secondary-gradient-light);
 		}
 		
 		&.danger {
-			background: var(--color-danger);
-			color: white;
+			background: var(--danger-gradient-light);
 		}
 		
 		&[disabled] {
---
+++ src/lib/types.d.ts
@@ -0,0 +1,1 @@
+export type SemanticColor = 'primary'|'secondary'|'danger'|'neutral';
